979fa253847f13e7f32a2a42f154a14471aaa5f0
Brian Waldon <brian@waldon.cc>
2015-02-24 12:06:34 -0800

    server: serve multiple ACIs; add flags

--- server.go
+++ server.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path"
+	"strings"
 	"text/template"
 )
 
-var t = template.Must(template.New("name").Parse(`<!DOCTYPE html>
+func main() {
+	fs := flag.NewFlagSet("aci-server", flag.ExitOnError)
+	domain := fs.String("domain", "", "")
+	listen := fs.String("listen", ":80", "")
+	dir := fs.String("image-dir", "/opt/images", "")
+	images := fs.String("images", "", "comma-delimited list of images to serve from --image-dir")
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		log.Fatalf("Failed parsing flags: %v", err)
+	}
+
+	if *domain == "" {
+		log.Fatalf("--domain must be set")
+	}
+
+	spImages := strings.Split(*images, ",")
+	if len(spImages) == 0 {
+		log.Fatalf("--images must be set")
+	}
+
+	log.Printf("Serving images: %v", spImages)
+
+	http.HandleFunc("/", handleDiscoverFunc(*domain, spImages))
+	http.Handle("/pubkeys.gpg", http.StripPrefix("/", http.FileServer(http.Dir(*dir))))
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir(*dir))))
+	log.Fatal(http.ListenAndServe(*listen, nil))
+}
+
+var tmpl = template.Must(template.New("name").Parse(`<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -16,30 +49,39 @@ var t = template.Must(template.New("name").Parse(`<!DOCTYPE html>
 <html>
 `))
 
-func discover(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	if r.URL.Query().Get("ac-discovery") != "1" {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	data := struct {
-		PrefixMatch    string
-		ACITemplateURL string
-		PubkeysURL     string
-	}{
-		"example.com/hello",
-		"http://example.com/images/{name}-{version}-{os}-{arch}.{ext}",
-		"http://example.com/pubkeys.gpg",
-	}
-	err := t.Execute(w, data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
+type entry struct {
+	PrefixMatch    string
+	ACITemplateURL string
+	PubkeysURL     string
 }
 
-func main() {
-	http.HandleFunc("/", discover)
-	http.Handle("/pubkeys.gpg", http.StripPrefix("/", http.FileServer(http.Dir("/opt/images"))))
-	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("/opt/images"))))
-	log.Fatal(http.ListenAndServe(":80", nil))
+func handleDiscoverFunc(domain string, images []string) http.HandlerFunc {
+	im := make(map[string]struct{}, len(images))
+	for _, image := range images {
+		im[image] = struct{}{}
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("ac-discovery") != "1" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		lookup := path.Base(r.URL.Path)
+		if _, ok := im[lookup]; !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		data := entry{
+			PrefixMatch:    fmt.Sprintf("%s/%s", domain, lookup),
+			ACITemplateURL: fmt.Sprintf("http://%s/images/%s-{version}-{os}-{arch}.{ext}", domain, lookup),
+			PubkeysURL:     fmt.Sprintf("http://%s/pubkeys.gpg", domain),
+		}
+
+		if err := tmpl.Execute(w, data); err != nil {
+			log.Printf("Failed serving discovery resource: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
 }
